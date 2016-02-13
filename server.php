801218fa237c68912c3c49d72bedfdbe7a514131
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2016-02-13 13:20:50 +0600

    Cleanup

--- server.php
+++ server.php
@@ -3,7 +3,9 @@
 namespace tutorial\php;
 
 error_reporting(E_ALL);
+ini_set("display_errors", "stderr");
 
+require_once 'vendor/autoload.php';
 require_once __DIR__.'/vendor/apache/thrift/lib/php/lib/Thrift/ClassLoader/ThriftClassLoader.php';
 
 use Thrift\ClassLoader\ThriftClassLoader;
@@ -15,9 +17,6 @@ $loader->registerNamespace('Thrift', __DIR__ . '/vendor/apache/thrift/lib/php/li
 $loader->registerDefinition('Example', $GEN_DIR);
 $loader->register();
 
-if (php_sapi_name() == 'cli') {
-  ini_set("display_errors", "stderr");
-}
 
 use Thrift\Factory\TBinaryProtocolFactory;
 use Thrift\Factory\TTransportFactory;
@@ -49,14 +48,16 @@ class ExampleService implements \Example\ExampleServiceIf {
   }
 }
 
-header('Content-Type', 'application/x-thrift');
-if (php_sapi_name() == 'cli') {
-  echo "\r\n";
-}
-
 $handler = new ExampleService();
 $processor = new \Example\ExampleServiceProcessor($handler);
 
 $transport = new TServerSocket();
-$server = new TSimpleServer($processor, $transport, new TTransportFactory(), new TTransportFactory(), new TBinaryProtocolFactory(), new TBinaryProtocolFactory());
+$server = new TSimpleServer(
+    $processor,
+    $transport,
+    new TTransportFactory(),
+    new TTransportFactory(),
+    new TBinaryProtocolFactory(),
+    new TBinaryProtocolFactory()
+);
 $server->serve();
